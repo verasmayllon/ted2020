284035338112e59d987f48c21ea0eeba24a73480
Mayllon Veras <veras@ifpi.edu.br>
2021-02-12 09:11:09 -0300

    Projeto criado

---
+++ src/main/java/br/edu/ifpi/ted/TedApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedApplication.class, args);
+	}
+
+}
