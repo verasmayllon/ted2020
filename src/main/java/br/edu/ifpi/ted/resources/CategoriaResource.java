c6986fd017ca507649c8f9366aeb4d96edb3cc69
Mayllon Veras <veras@ifpi.edu.br>
2021-02-21 17:22:25 -0300

    Banco H2 conectado

--- src/main/java/br/edu/ifpi/ted/resources/CategoriaResource.java
+++ src/main/java/br/edu/ifpi/ted/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.edu.ifpi.ted.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifpi.ted.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 }
